1293ee43836908dc5c061cb9eac57e46f187cce7
mdz48 <maximilianodiazclemen.cetis138@gmail.com>
2024-10-30 03:22:14 -0600

    Compatible

--- src/persistencia/models/User.ts
+++ src/persistencia/models/User.ts
@@ -1,12 +1,25 @@
 export class User {
-    private id: number;
-    private name: string;
-    private lastName: string;
-
-    constructor(id:number, name:string, lastname: string){
-        this.id = id
-        this.name = name;
-        this.lastName = lastname
+    private iduser: number;
+    private mail: string;
+    private password: string;
+    private weight: number;
+    private height: number;
+    private sex: string;
+    constructor(iduser:number, mail:string, password: string, sex: string, weight: number, height: number){
+        this.iduser = iduser
+        this.mail = mail;
+        this.password = password;
+        this.weight = weight;
+        this.height = height;
+        this.sex = sex;
     }
 }
-  
+
+export interface UserInterface {
+    iduser: number;
+    mail: string;
+    password: string;
+    sex: string;
+    weight: number;
+    height: number;
+}
--- src/persistencia/repositorios/UserRepository.ts
+++ src/persistencia/repositorios/UserRepository.ts
@@ -1,6 +1,6 @@
 import dotenv from 'dotenv'
-import mysql from 'mysql2/promise';
-import { User } from '../models/User';
+import mysql, { ResultSetHeader } from 'mysql2/promise';
+import { User, UserInterface } from '../models/User';
 dotenv.config();
 
 export class UserRepository {
@@ -26,24 +26,38 @@ export class UserRepository {
 
     async getId(id:number): Promise<User | null> {      
         
-        try {
-            const [rows]:any = await this.connection.execute('SELECT * FROM users WHERE id=?',[id]);
-            return new User(
-                rows[0].id,
-                rows[0].name,
-                rows[0].lastname,
-              );
-        } catch (error) {
-              return null;
-        }       
+    //     try {
+    //         const [rows]:any = await this.connection.execute('SELECT * FROM users WHERE id=?',[id]);
+    //         return new User(
+    //             rows[0].id,
+    //             rows[0].name,
+    //             rows[0].lastname,
+    //           );
+    //     } catch (error) {
+    //           return null;
+    //     }    
+    return null   
     }
+
+    // async getMail(mail:string): Promise<User | null> {
+    //     try {
+    //         const [rows]:any = await this.connection.execute('SELECT * FROM users WHERE mail=?',[mail]);
+    //         return new User(
+    //             rows[0].id,
+    //             rows[0].name,
+    //             rows[0].lastname,
+    //           );
+    //     } catch (error) {
+    //         return null;
+    //     }
+    // }   
     
     async createNewUser(data:any): Promise<User | null> {
         let user = null
         
         try {
-            const [result]:any = await this.connection.execute('INSERT INTO users (name, lastname) VALUES (?, ?)',[data.name,data.lastname]);
-            return new User(result.insertId, data.name, data.lastname) 
+            const [result]:any = await this.connection.execute('INSERT INTO users (mail, password, height, weight, sex) VALUES (?, ?, ?, ?, ?)',[data.mail,data.password, data.height, data.weight, data.sex]);
+            return new User(result.insertId, data.mail, data.password, data.height, data.weight, data.sex) 
         }
         catch(error){
             return null
@@ -52,13 +66,14 @@ export class UserRepository {
     }
 
     async updateUser(id:number, data:any): Promise<User | null> {        
-        try {
-            const [result] = await this.connection.execute('UPDATE users SET name=?, lastname=? WHERE id=?',[data.name,data.lastname,id]);
-            return new User(id, data.name, data.lastname) 
-        }
-        catch(error){            
-            return null
-        }   
+        // try {
+        //     const [result] = await this.connection.execute('UPDATE users SET name=?, lastname=? WHERE id=?',[data.name,data.lastname,id]);
+        //     return new User(id, data.name, data.lastname) 
+        // }
+        // catch(error){            
+        //     return null
+        // }   
+    return null
     }
 
     async updateUserPartial(id:number,data:any): Promise<User | null> {
